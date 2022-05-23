6bcf1f391b26b14a97a7d1bfddb94b15d35b1c46
victorfborges <victorfborges@github.com>
2022-05-23 19:58:24 -0300

    first commit

---
+++ ex1/cliente.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define SERVER_PORT 54321
+#define MAX_LINE 256
+
+int main (int argc, char *argv[])
+{
+	FILE *fp;
+	struct hostent *hp;
+	struct sockaddr_in sin; 
+	char *host;
+	char buf[MAX_LINE];
+	int s;
+	int len;
+	struct in_addr addr;
+	char input_text[100];
+	char final_text[100];
+	
+	/* abertura e leitura de arquivo de entrada */
+	if((fp=fopen("input.txt","r"))==NULL)
+		printf("file open fail\n");
+	else
+		printf("file open sucess\n");
+
+	//while(fgets(input_text, 100, fp) != NULL){
+	//	printf("%s\n", input_text); 
+	//}
+	
+	/* verificação da passagem do endereço IP como parametro */
+	if(argc==3){
+		host=argv[1];
+		printf("ponto1\n");
+	}		
+	else{
+		fprintf(stderr, "usage: simples-talk host\n");
+		exit(1);
+	}
+	
+	/* verificação da passagem do numero do cliente como parametros */
+	if(argc==3){
+		*final_text=argv[2];
+	}		
+	else{
+		printf("client number not available\n");
+		exit(1);
+	}
+	
+	/* montagem da mensagem lida do arquivo */
+	strcat(final_text, input_text);
+	
+	/*traduz nome do host para endereço IP do host*/
+	inet_aton(host, &addr);
+	hp = gethostbyaddr(&addr, sizeof(addr), AF_INET);
+	if(!hp){
+		fprintf(stderr, "simplex-talk: unknown host: %s\n", host);
+		exit(1);
+	}
+	
+	/*monta estrutura de dados do endereço*/
+	bzero((char *)&sin, sizeof(sin));
+	sin.sin_family = AF_INET;
+	bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+	sin.sin_port = htons(SERVER_PORT);
+	
+	
+	/*abertura ativa*/
+	if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0){
+		perror("simplex-talk: socket");
+		exit(1); 
+	}
+	if(connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+	{
+		perror("simplex-talk: connect");
+		close(s);
+		exit(1); 
+	}
+	
+	printf("carai\n");
+	/* laço principal: obtém e envia linhas de texto */
+	while (fgets(buf, MAX_LINE, fp) != NULL){
+		buf[MAX_LINE-1] = '\0';
+		len = strlen(buf) + 1;
+		send(s, buf, len, 0);
+		printf("%s\n", buf);
+	}
+	fclose(fp);
+}
